d1bd584b7b40c018f81ffe4f3010e2cee8936c7d
Luke Hindman <lukehindman@boisestate.edu>
2020-10-14 13:11:13 -0700

    Lecture examples added

--- PointerBasics/main.c
+++ PointerBasics/main.c
@@ -2,7 +2,44 @@
 
 int main(void) {
 
-   /* Your code here */
+   /*
+    * Pointer basics
+    */
+
+   printf("*\n* Pointer Basics\n*\n");
+   /* Start by declaring a variable to hold a int value */
+   int roomCapacity = 111000000;
+
+   /* Store the address of that variable in an int * variable (int pointer) */
+   int * capacityPtr = &roomCapacity;
+
+   /* Use the dereference operator ( * ) to access the data stored at the pointer address */
+   printf("The room capacity is: %d\n", *capacityPtr);
+
+
+   /*
+    * Pointers generally are declared with a type such as int *, long * or char *.  This
+    *    allows C to know the proper data type and number of bytes to process when
+    *    applying the dereference operator.  However it is possible to have a pointer 
+    *    with no assocated data type, void * (void pointer).  Since a void pointer does
+    *    not have an associated type, it must be cast to the proper pointer type 
+    *    before being deferenced.  It is incorrect to attempt to dereference a void pointer.
+    */
+   printf("\n*\n* Void Pointers and Casting\n*\n");
+   void * data = &roomCapacity;
+
+   char * charDataPtr = (char *) data;
+
+   printf("The data value as a char is: %c\n", *charDataPtr);
+
+   int * intDataPtr = (int * ) data;
+
+   printf("The data value as an int is: %d\n", *intDataPtr);
+
+   double * dblDataPtr = (double *) data;
+
+   printf("The data value as a double is: %lf\n", *dblDataPtr);
+
 
    return 0;
 }
--- PointersAndArrays/main.c
+++ PointersAndArrays/main.c
@@ -2,7 +2,51 @@
 
 int main(void) {
 
-   /* Your code here */
+   /* Lets start with an array of chars, also known as a string */
+   char phrase[] = "Life is like a box of chocolates";
+
+   // printf("char using array syntax:  %c\n", phrase[6]);
+   // printf("char using pointer syntax:  %c\n", *(phrase + 6));
+
+   char * myFirstCharPtr = phrase + 0;
+   char * mySecondCharPtr = phrase + 2;
+
+   printf("First Char: %c\n", *myFirstCharPtr);
+   printf("Second Char: %c\n", *mySecondCharPtr);
+
+   printf("Address of First Char: %p\n", myFirstCharPtr);
+   printf("Address of Second Char: %p\n", mySecondCharPtr);
+   
+
+   /* Let's look at the same thing again, but with an array of ints instead of chars */ 
+   int numbers[10];
+   for (int i = 0; i < 10; i++) {
+      numbers[i] = i;
+   }
+
+   int * myFirstIntPtr = numbers;
+   int * mySecondIntPtr = numbers + 2;
+
+   printf("First Int:  %d\n", *myFirstIntPtr);
+   printf("Second Int:  %d\n", *mySecondIntPtr);
+
+   printf("Address of First Int: %p\n", myFirstIntPtr);
+   printf("Address of Second Int: %p\n", mySecondIntPtr);
+
+   /* We can also use the ++/-- operators as a shorthand for walking through
+    * the elements of an array using pointers */
+   char * index = phrase;
+   printf("forward using ++: ");
+   while (*index != '\0') {
+      printf("%c", *index++);
+   }
+   printf("\n");
+
+   printf("reverse using --: ");
+   while (index >= phrase) {
+      printf("%c", *index--);
+   }
+   printf("\n");
 
    return 0;
 }
